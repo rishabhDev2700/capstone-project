28697a968d696e085563f9d81d3c30c8fdf84ca0
rishabhDev2700 <rishavkumar2700@gmail.com>
2023-02-06 20:09:05 +0530

    removed extra files and added tailwindcss config

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { TodoList } from "./components/TodoList";
+import { Topbar } from "./components/Topbar";
+import { motion } from "framer-motion";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Completed } from "./components/Completed";
+import { Incomplete } from "./components/Incomplete";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <motion.div
+      className="bg-red-200"
+      initial={{ opacity: 0, x: -700 }}
+      whileInView={{ opacity: 100, x: 0 }}
+      transition={{ duration: 1.5 }}
+    >
+      <BrowserRouter>
+      <Topbar />
+        <Routes>
+        <Route path="/" element={<TodoList />} />
+        <Route path="/completed" element={<Completed />} />
+        <Route path="/incomplete" element={<Incomplete />} />
+        </Routes>
+      </BrowserRouter>
+    </motion.div>
   );
 }
 
