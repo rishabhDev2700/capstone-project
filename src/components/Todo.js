0d3295f45502bd99952f96a279ee808019f763b3
rishabhDev2700 <rishavkumar2700@gmail.com>
2023-02-08 00:09:27 +0530

    Added animation to todo component and added some sample data in todolist

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,16 +1,16 @@
-import React from 'react'
-
+import {motion} from 'framer-motion';
 export const Todo = (props) => {
   return (
-    <div className="flex items-center w-2/3 border-2 border-blue-200 bg-white rounded-md m-2">
-        <input className="form-checkbox mx-8 p-4" type="checkbox" {...`${props.done?"?":"d"}`}/>
+    <motion.div initial={{x:1200}} whileInView={{x:0}} transition={{duration:2.5}}
+    className="flex items-center w-2/3 border-2 border-blue-200 bg-white rounded-md m-2 p-4">
+        <input className="form-checkbox mx-8 p-4" type="checkbox" />
         <div>
           <div className="text-2xl flex justify-evenly">
             <div className="font-semibold">{props.title}</div>
-            <div>{props.date.toString()}</div>
+            <div>{props.date.toDateString()}</div>
           </div>
           <div className="text-xl">{props.note}</div>
         </div>
-      </div>
+      </motion.div>
   )
 }
