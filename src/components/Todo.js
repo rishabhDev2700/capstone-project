208a807e6a04ede5bdf17165f86802a7ca613bae
rishabhDev2700 <rishavkumar2700@gmail.com>
2023-02-10 02:47:23 +0530

    Customized layout and added toDateString function for displaying date

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,16 +1,19 @@
 import {motion} from 'framer-motion';
+import { GrTrash } from 'react-icons/gr';
 export const Todo = (props) => {
+  const date = new Date(props.date);
   return (
     <motion.div initial={{x:1200}} whileInView={{x:0}} transition={{duration:2.5}}
-    className="flex items-center w-2/3 border-2 border-blue-200 bg-white rounded-md m-2 p-4">
+    className="flex justify-between items-center w-2/3 border-2 border-blue-200 bg-white rounded-md m-2 p-4">
         <input className="form-checkbox mx-8 p-4" type="checkbox" />
-        <div>
-          <div className="text-2xl flex justify-evenly">
+        <div className='flex flex-col'>
+          <div className="text-2xl w-full">
             <div className="font-semibold">{props.title}</div>
-            <div>{props.date.toDateString()}</div>
+            <div>{date.toDateString()}</div>
           </div>
           <div className="text-xl">{props.note}</div>
         </div>
+        <GrTrash className='scale-150 mr-8'/>
       </motion.div>
   )
 }
